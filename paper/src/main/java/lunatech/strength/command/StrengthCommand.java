a6ed89afe402cb1c83de7b1b02d1b5e6ac3d4469
lunarenzo <luna.renzo.zamora@gmail.com>
2026-08-19 23:40:58 +0800

    feat(command): add admin commands /strength changeweapon <player> <weapon> and /strength set <player> <amount>

--- paper/src/main/java/lunatech/strength/command/StrengthCommand.java
+++ paper/src/main/java/lunatech/strength/command/StrengthCommand.java
@@ -1,7 +1,10 @@
 package lunatech.strength.command;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.IntegerArgument;
+import dev.jorel.commandapi.arguments.StringArgument;
 import dev.jorel.commandapi.executors.CommandArguments;
 import lunatech.strength.AbstractStrength;
 import lunatech.strength.service.StrengthService;
@@ -9,6 +12,8 @@ import io.github.milkdrinkers.colorparser.paper.ColorParser;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
 
 import static lunatech.strength.command.CommandHandler.BASE_PERM;
 
@@ -37,6 +42,22 @@ final class StrengthCommand extends Command {
                     .withHelp("Withdraw strength into a physical item.", "Withdraw strength into a physical item.")
                     .withArguments(new IntegerArgument("amount", 1))
                     .executesPlayer(this::executorWithdraw),
+                new CommandAPICommand("changeweapon")
+                    .withHelp("Change a target player's assigned weapon.", "Change a target player's assigned weapon.")
+                    .withPermission(BASE_PERM + ".changeweapon")
+                    .withArguments(
+                        new EntitySelectorArgument.OnePlayer("target"),
+                        new StringArgument("weapon").replaceSuggestions(ArgumentSuggestions.stringCollection(info -> List.of("sword", "axe", "bow", "crossbow", "trident", "mace", "shield")))
+                    )
+                    .executes(this::executorChangeWeapon),
+                new CommandAPICommand("set")
+                    .withHelp("Set a target player's strength level.", "Set a target player's strength level.")
+                    .withPermission(BASE_PERM + ".set")
+                    .withArguments(
+                        new EntitySelectorArgument.OnePlayer("target"),
+                        new IntegerArgument("amount", 0)
+                    )
+                    .executes(this::executorSetStrength),
                 new CommandAPICommand("reload")
                     .withHelp("Reload the plugin configuration and translations.", "Reload the plugin configuration and translations.")
                     .withPermission(BASE_PERM + ".reload")
@@ -97,6 +118,70 @@ final class StrengthCommand extends Command {
 
         player.sendMessage(
             ColorParser.of("<green>Successfully withdrew <amount> Strength into a physical item!</green>")
+                .with("amount", String.valueOf(amount))
+                .build()
+        );
+    }
+
+    private void executorChangeWeapon(CommandSender sender, CommandArguments args) {
+        final Player target = (Player) args.get("target");
+        final String weapon = ((String) args.get("weapon")).toLowerCase();
+        final List<String> availableWeapons = List.of("sword", "axe", "bow", "crossbow", "trident", "mace", "shield");
+
+        if (target == null) {
+            sender.sendMessage(ColorParser.of("<red>Target player not found or offline!</red>").build());
+            return;
+        }
+
+        if (!availableWeapons.contains(weapon)) {
+            sender.sendMessage(
+                ColorParser.of("<red>Invalid weapon type! Available: <list></red>")
+                    .with("list", String.join(", ", availableWeapons))
+                    .build()
+            );
+            return;
+        }
+
+        final StrengthService strengthService = plugin.getStrengthService();
+        strengthService.setAssignedWeapon(target, weapon);
+
+        sender.sendMessage(
+            ColorParser.of("<green>Successfully changed <target>'s assigned weapon to <weapon>!</green>")
+                .with("target", target.getName())
+                .with("weapon", weapon.toUpperCase())
+                .build()
+        );
+
+        target.sendMessage(
+            ColorParser.of("<gold>Your assigned weapon has been set to <weapon> by an admin!</gold>")
+                .with("weapon", weapon.toUpperCase())
+                .build()
+        );
+    }
+
+    private void executorSetStrength(CommandSender sender, CommandArguments args) {
+        final Player target = (Player) args.get("target");
+        final int amount = (int) args.get("amount");
+
+        if (target == null) {
+            sender.sendMessage(ColorParser.of("<red>Target player not found or offline!</red>").build());
+            return;
+        }
+
+        final StrengthService strengthService = plugin.getStrengthService();
+        final int oldStrength = strengthService.getStrength(target);
+        strengthService.setStrength(target, amount);
+
+        sender.sendMessage(
+            ColorParser.of("<green>Successfully set <target>'s strength from <old> to <amount>!</green>")
+                .with("target", target.getName())
+                .with("old", String.valueOf(oldStrength))
+                .with("amount", String.valueOf(amount))
+                .build()
+        );
+
+        target.sendMessage(
+            ColorParser.of("<gold>Your strength level was set to <amount> by an admin!</gold>")
                 .with("amount", String.valueOf(amount))
                 .build()
         );
