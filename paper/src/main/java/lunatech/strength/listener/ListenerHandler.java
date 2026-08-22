8465a288197d3c3b9d0a83134ff990c0fe50c5b2
lunarenzo <luna.renzo.zamora@gmail.com>
2026-08-23 02:20:36 +0800

    fix(worldguard): pass Strength plugin instance to PlayerKillListener constructor for WorldGuardHook query

--- paper/src/main/java/lunatech/strength/listener/ListenerHandler.java
+++ paper/src/main/java/lunatech/strength/listener/ListenerHandler.java
@@ -28,7 +28,7 @@ public class ListenerHandler implements Reloadable {
     public void onEnable(AbstractStrength plugin) {
         listeners.clear(); // Clear the list to avoid duplicate listeners when reloading the plugin
         
-        listeners.add(new lunatech.strength.listener.player.PlayerKillListener(this.plugin.getStrengthService(), this.plugin.getConfigHandler()));
+        listeners.add(new lunatech.strength.listener.player.PlayerKillListener(this.plugin, this.plugin.getStrengthService(), this.plugin.getConfigHandler()));
         listeners.add(new lunatech.strength.listener.player.PlayerJoinListener(this.plugin, this.plugin.getStrengthService()));
         listeners.add(new lunatech.strength.listener.player.StrengthConsumeListener(this.plugin.getStrengthService(), this.plugin.getConfigHandler()));
         listeners.add(new lunatech.strength.listener.player.RerollConsumeListener(this.plugin));
