9f6139e3aa46f42a7815fe53b5bb5e23e3d846b4
lunarenzo <luna.renzo.zamora@gmail.com>
2026-08-28 20:47:35 +0800

    fix(worldguard): evaluate DENY explicitly via queryState to allow abilities in wilderness by default

--- paper/src/main/java/lunatech/strength/integration/WorldGuardHook.java
+++ paper/src/main/java/lunatech/strength/integration/WorldGuardHook.java
@@ -70,10 +70,10 @@ public final class WorldGuardHook {
             com.sk89q.worldedit.util.Location weLoc = BukkitAdapter.adapt(loc);
             com.sk89q.worldguard.LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
 
-            if (!query.testState(weLoc, localPlayer, Flags.PVP)) {
+            if (query.queryState(weLoc, localPlayer, Flags.PVP) == StateFlag.State.DENY) {
                 return false;
             }
-            if (STRENGTH_WEAPON_ABILITIES != null && !query.testState(weLoc, localPlayer, STRENGTH_WEAPON_ABILITIES)) {
+            if (STRENGTH_WEAPON_ABILITIES != null && query.queryState(weLoc, localPlayer, STRENGTH_WEAPON_ABILITIES) == StateFlag.State.DENY) {
                 return false;
             }
         } catch (Throwable ignored) {
@@ -94,7 +94,7 @@ public final class WorldGuardHook {
             com.sk89q.worldedit.util.Location weLoc = BukkitAdapter.adapt(loc);
             com.sk89q.worldguard.LocalPlayer localPlayer = victim != null ? WorldGuardPlugin.inst().wrapPlayer(victim) : null;
 
-            if (STRENGTH_PVP_LOSS != null && !query.testState(weLoc, localPlayer, STRENGTH_PVP_LOSS)) {
+            if (STRENGTH_PVP_LOSS != null && query.queryState(weLoc, localPlayer, STRENGTH_PVP_LOSS) == StateFlag.State.DENY) {
                 return false;
             }
         } catch (Throwable ignored) {
@@ -115,7 +115,7 @@ public final class WorldGuardHook {
             com.sk89q.worldedit.util.Location weLoc = BukkitAdapter.adapt(loc);
             com.sk89q.worldguard.LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
 
-            if (STRENGTH_REROLL != null && !query.testState(weLoc, localPlayer, STRENGTH_REROLL)) {
+            if (STRENGTH_REROLL != null && query.queryState(weLoc, localPlayer, STRENGTH_REROLL) == StateFlag.State.DENY) {
                 return false;
             }
         } catch (Throwable ignored) {
