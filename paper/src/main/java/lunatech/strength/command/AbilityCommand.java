c45dd6df2db4bb94e5336a2a5343807fd1a17497
lunarenzo <luna.renzo.zamora@gmail.com>
2026-09-07 22:43:16 +0800

    fix(worldguard): add plugin enabled check before calling WorldGuardHook in AbilityCommand to prevent NoClassDefFoundError

--- paper/src/main/java/lunatech/strength/command/AbilityCommand.java
+++ paper/src/main/java/lunatech/strength/command/AbilityCommand.java
@@ -67,9 +67,11 @@ public final class AbilityCommand extends Command {
             return;
         }
 
-        if (!lunatech.strength.integration.WorldGuardHook.isAbilityAllowed(plugin, player, player.getLocation())) {
-            player.sendMessage(ColorParser.of("<red>You cannot use weapon abilities in this region!</red>").build());
-            return;
+        if (plugin.getServer().getPluginManager().isPluginEnabled("WorldGuard")) {
+            if (!lunatech.strength.integration.WorldGuardHook.isAbilityAllowed(plugin, player, player.getLocation())) {
+                player.sendMessage(ColorParser.of("<red>You cannot use weapon abilities in this region!</red>").build());
+                return;
+            }
         }
 
         if ("trident".equalsIgnoreCase(assignedWeapon)) {
